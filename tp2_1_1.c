7b4d797128c139c915c82f71a4d01583dcff22c3
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-03-31 17:47:50 -0300

    primera insercion del .gitignore y tp2_1_1.c

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+int main()
+{
+	int i;
+	double vt[N];
+	
+	for(i=0; i<N; i++)
+	{
+		vt[i] = 1 + rand()%100;
+		printf("%.2lf\n", vt[i]);
+	}
+
+	return 0;	
+}
